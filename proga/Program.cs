5ccc43bf74aa0c74fb6f9144efb0d08944ce6f41
drama <loginovzaxar04@gmail.com>
2023-01-11 11:48:32 +0300

    first comit

--- proga/Program.cs
+++ proga/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace proga
+namespace JABKA
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+
+
+            Person tom = new Person();  
+
+
+            string personName = tom.name;
+            int personAge = tom.age;
+            Console.WriteLine($"Имя: {personName}  Возраст {personAge}");
+
+
+            tom.name = Console.ReadLine();
+            tom.age = ThingAboutThis("Print: ");
+            tom.Print();
+
+
+        }
+
+        static int ThingAboutThis(string message)
+        {
+            int znach;
+            while (true)
+            {
+                Console.Write(message);
+                var chislo = Console.ReadLine();
+                bool result = int.TryParse(chislo, out znach);
+                if (result)
+                {
+                    break;
+                }
+                Console.WriteLine("Exception");
+            }
+              return znach;
         }
     }
 }
---
+++ proga/tom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JABKA
+{
+    class Person
+    {
+        public string name = "Undefined";
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя:{name} Возраст: {age} ");
+        }
+
+
+    }
+}
